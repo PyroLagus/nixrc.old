bc07fcba4d72d9b70929d3262f03405123480f34
C0DEHERO <codehero@nerdpol.ch>
2015-04-29 17:47:42 -0400

    updated system activation script

--- configuration-common.nix
+++ configuration-common.nix
@@ -51,28 +51,30 @@ rec {
       ln -fs ${./dotfiles/bash_profile} .bash_profile
       ln -fs ${./dotfiles/bashrc} .bashrc
       ln -fsn ${./dotfiles/bin} bin
+      mkdir .cabal 2>/dev/null || true
+      chown codehero:users .cabal
+      ln -fs ${./dotfiles/cabal/config} .cabal/config
+      ln -fs ${./dotfiles/dmrc} .dmrc
+      mkdir .emacs.d 2>/dev/null || true
+      chown codehero:users .emacs.d
+      ln -fs ${./dotfiles/emacs.d/emacs.org} .emacs.d/emacs.org
+      ln -fs ${./dotfiles/emacs.d/init.el} .emacs.d/init.el
+      ln -fs ${./dotfiles/fehbg} .fehbg
+      ln -fs ${./dotfiles/functions} .functions
       ln -fs ${./dotfiles/gitconfig} .gitconfig
       mkdir .nixpkgs 2>/dev/null || true
-      chown codehero:codehero .nixpkgs
+      chown codehero:users .nixpkgs
       ln -fs ${./dotfiles/nixpkgs/config.nix} .nixpkgs/config.nix
-      mkdir .emacs.d 2>/dev/null || true
-      chown codehero:codehero .emacs.d
-      ln -fs ${./dotfiles/emacs.d/init.el} .emacs.d/init.el
-      ln -fs ${./dotfiles/emacs.d/emacs.org} .emacs.d/emacs.org
-      ln -fs ${./dotfiles/xmobarrc} .xmobarrc
-      ln -fs ${./dotfiles/dmrc} .dmrc
-      ln -fs ${./dotfiles/fehbg} .fehbg
-      mkdir .cabal 2>/dev/null || true
-      ln -fs ${./dotfiles/cabal/config} .cabal/config
-      ln -fs ${./dotfiles/Xresources} .Xresources
-      ln -fs ${./dotfiles/zshrc} .zshrc
-      ln -fs ${./dotfiles/zshrc.local} .zshrc.local
       mkdir .pulse 2>/dev/null || true
-      chown codehero:codehero .pulse
+      chown codehero:users .pulse
       ln -fs ${./dotfiles/pulse/default.pa} .pulse/default.pa
+      ln -fs ${./dotfiles/xmobarrc} .xmobarrc
       mkdir .xmonad 2>/dev/null || true
-      chown codehero:codehero .xmonad
+      chown codehero:users .xmonad
       ln -fs ${./dotfiles/xmonad/xmonad.hs} .xmonad/xmonad.hs
+      ln -fs ${./dotfiles/zshrc.local} .zshrc.local
+      ln -fs ${./dotfiles/zshrc} .zshrc
+      ln -fs ${./dotfiles/Xresources} .Xresources
     '';
   };
 }
